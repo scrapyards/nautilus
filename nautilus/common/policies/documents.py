e558f8462f0489f61785bcabb6a2e9a0d5be566e
tin.l.lam <tinlam@gmail.com>
2017-08-02 22:17:27 -0500

    WIP: Add oslo and keystoneauth integration
    This patch set starts the integration of keystone middleware
    authentication and oslo library integration.
    
    Change-Id: I05238ec09261239db7c02381e20ae36a21e46f46

---
+++ nautilus/common/policies/documents.py
@@ -0,0 +1,36 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from oslo_policy import policy
+
+from nautilus.common.policies import base
+
+
+documents_policies =[
+    policy.DocumentedRuleDefault(
+        name=base.NAUTILUS % 'get_documents',
+        check_str=base.RULE_SERVICE_OR_ADMIN,
+        description='List documents',
+        operations=[{'path': '/v1/documents/',
+                     'method': 'GET'}]),
+
+    policy.DocumentedRuleDefault(
+        name=base.NAUTILUS % 'get_documents',
+        check_str=base.RULE_ADMIN_REQUIRED,
+        description='List documents',
+        operations=[{'path': '/v1/documents/',
+                     'method': 'POST'}]),
+]
+
+
+def list_rules():
+    return documents_policies
---
+++ nautilus/service.py
@@ -0,0 +1,27 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import falcon
+from oslo_config import cfg
+from paste import deploy
+from paste import urlmap
+
+from nautilus.comm import config
+
+CONF = cfg.CONF
+
+
+def initialize(conf=None, name='main'):
+    config.parse_args(args=sys.argv[1:])
+    config.setup_logging()
+    conf = config.find_paste_config()
+    return deploy.loadapp('config:%s' % conf, name=name)
