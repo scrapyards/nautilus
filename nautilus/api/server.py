6c2db8138adfdf904fb0f57ab3b0fd2bd08e83bd
tin.l.lam <tinlam@gmail.com>
2017-08-02 22:17:27 -0500

    WIP: Add oslo and keystoneauth integration
    This patch set starts the integration of keystone middleware
    authentication and oslo library integration.
    
    Change-Id: I05238ec09261239db7c02381e20ae36a21e46f46

---
+++ nautilus/api/server.py
@@ -0,0 +1,68 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging as py_logging
+import os
+
+import falcon
+from oslo_config import cfg
+from oslo_log import log
+from paste import deploy
+from paste import urlmap
+
+from nautilus.api import request
+from nautilus.common.i18n import _
+
+
+CONF = cfg.CONF
+LOG = log.getLogger(__name__)
+
+
+def setup_logging(conf):
+
+    # Add additional dependent libraries that have unhelp bug levels
+    extra_log_level_defaults = []
+
+    log.set_defaults(default_log_levels=log.get_default_log_levels() +
+                     extra_log_level_defaults)
+    log.setup(conf, 'nautilus')
+    py_logging.captureWarnings(True)
+
+
+def init_application(base=None, **kwargs):
+    config_file = kwargs.get('config_file', 'nautilus.conf')
+    config_path = kwargs.get('config_path', None)
+    paste_file = kwargs.get('paste_file', 'nautilus-paste.ini')
+
+    # TODO(lamt) Enable the logging load
+    # setup_logging(CONF)
+
+    if config_path is None:
+        config_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), '../../etc')
+
+    config_file = os.path.join(config_path, config_file)
+    global_config = {'config_file': config_file}
+
+    LOG.debug(_('Starting WSGI application using configuration from %s'),
+              config_path)
+
+    return deploy.loadapp('config:%s' % paste_file,
+                          relative_to=config_path)
+
+
+if __name__ == '__main__':
+    from wsgiref import simple_server
+    # TODO(lamt) Need to remove this hardcode bit and use search
+    wsgi_app = init_application(base='/Users/tlam/proj/etc/nautilus')
+    httpd = simple_server.make_server('127.0.0.1', 9900, wsgi_app)
+    httpd.serve_forever()
