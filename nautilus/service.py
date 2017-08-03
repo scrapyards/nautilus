a97c26d7b871fdbaa40434fc16934cda8bcbee40
tin.l.lam <tinlam@gmail.com>
2017-08-02 22:17:27 -0500

    WIP: Add oslo and keystoneauth integration
    This patch set starts the integration of keystone middleware
    authentication and oslo library integration.
    
    Change-Id: I05238ec09261239db7c02381e20ae36a21e46f46

---
+++ nautilus/service.py
@@ -0,0 +1,44 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import functools
+import sys
+
+import falcon
+from oslo_log import log
+
+from nautilus import api
+from nautilus.api.common import middleware
+from nautilus.api import v1
+
+
+LOG = log.getLogger(__name__)
+
+
+def configure_app(app, version=''):
+
+    # TODO(lamt) Set up policy here
+    for path, res in v1.endpoints():
+        app.add_route('%s%s' % (version, path), res)
+
+    return app
+
+
+def nautilus_app_factory(global_config, **local_config):
+
+    middleware_list = list()
+    middleware_list.append(middleware.JSONTranslator())
+
+    app = falcon.API(request_type=api.request.Request,
+                     middleware=middleware_list)
+
+    return configure_app(app, version='/v1')
