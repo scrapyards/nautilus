202a253d88b24f43fa531c1785bdd97458b8cc1a
tin.l.lam <tinlam@gmail.com>
2017-08-02 22:17:27 -0500

    WIP: Add oslo and keystoneauth integration
    This patch set starts the integration of keystone middleware
    authentication and oslo library integration.
    
    Change-Id: I05238ec09261239db7c02381e20ae36a21e46f46

---
+++ nautilus/api/common/resource.py
@@ -0,0 +1,36 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+
+import falcon
+
+from nautilus import exceptions as exc
+
+
+class BaseResource(object):
+
+    @staticmethod
+    def json_body(req):
+        if not req.content_type:
+            return {}
+        try:
+            raw_json = req.stream.read()
+        except Exception:
+            raise exc.ObjectNotFound('Oh em gee, exception!')
+
+        try:
+            data = json.loads(raw_json, 'utf-8')
+        except ValueError:
+            raise falcon.HTTPError(falcon.HTTP_753, 'Malformed JSON')
+
+        return data
