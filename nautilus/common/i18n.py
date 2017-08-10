ce9eb4e6c1cda3be0cafb072afb2560f1002eaeb
Tin Lam <tin@irrational.io>
2017-08-10 00:12:29 -0500

    Fix i18n's domain
    This patch set fixes the i18n domain from 'keystone' to 'nautilus'.
    
    Change-Id: Ibe9b045bd23219b09ddbfda1273cd442c8229886

--- nautilus/common/i18n.py
+++ nautilus/common/i18n.py
@@ -13,7 +13,7 @@
 import oslo_i18n
 
 
-_translators = oslo_i18n.TranslatorFactory(domain='keystone')
+_translators = oslo_i18n.TranslatorFactory(domain='nautilus')
 
 # The primary translation function using the well-known name "_"
 _ = _translators.primary
